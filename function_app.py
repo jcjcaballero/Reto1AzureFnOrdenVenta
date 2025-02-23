2804ad96e2aee7d75cb7d33e94577b9580cefcfd
JonnathanCaballero <jcjcaballero@hotmail.com>
2025-02-23 10:39:06 -0500

    Se ajustan las conexiones.

--- function_app.py
+++ function_app.py
@@ -1,15 +1,23 @@
 import azure.functions as func
+from azure.cosmos import CosmosClient
+from azure.servicebus import ServiceBusClient
 import logging
 from services import insert_order
 from models import Order
+import os
 
 app = func.FunctionApp(http_auth_level=func.AuthLevel.FUNCTION)
+cosmos_db_connection_string = os.getenv("l_cosmos_db_connection_string")
+service_bus_connection_string = os.getenv("l_service_bus_connection_string")
+
+client = CosmosClient.from_connection_string(cosmos_db_connection_string)
+clienteBus = ServiceBusClient.from_connection_string(service_bus_connection_string)
 
 @app.route(route="registrarOrdenVenta")
 def registrarOrdenVenta(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Processing sell order request')
 
     order = Order.generate_random_order()
-    insert_order(order)
+    insert_order(order, client, clienteBus)
     
     return func.HttpResponse(f"Order {order.id} created successfully", status_code=201)
--- services.py
+++ services.py
@@ -11,17 +11,17 @@ container_name = os.getenv("l_container_name")
 service_bus_connection_string = os.getenv("l_service_bus_connection_string")
 queue_name = os.getenv("l_queue_name")
 
-def insert_order(order: Order):
-    client = CosmosClient.from_connection_string(cosmos_db_connection_string)
+
+def insert_order(order: Order, client, clienteBus):
+    ##client = CosmosClient.from_connection_string(cosmos_db_connection_string)
     database = client.get_database_client(cosmos_db_name)
     container = database.get_container_client(container_name)
     
     container.upsert_item(order.__dict__)
-    send_message_to_queue(order)
+    send_message_to_queue(order, clienteBus)
 
-def send_message_to_queue(order: Order):
-    message_body = json.dumps(order.__dict__)
-    with ServiceBusClient.from_connection_string(service_bus_connection_string) as client:
-        with client.get_queue_sender(queue_name) as sender:
-            message = ServiceBusMessage(message_body)
-            sender.send_messages(message)
+def send_message_to_queue(order: Order, client):
+    message_body = json.dumps(order.__dict__)    
+    sender = client.get_queue_sender(queue_name)
+    message = ServiceBusMessage(message_body)
+    sender.send_messages(message)
